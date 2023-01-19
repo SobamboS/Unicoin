3ebcce72e31c7249673de17fcde1337f926e1a62
SobamboS <sobamboolusola1152@gmail.com>
2023-01-19 15:48:13 +0100

    This is branch

--- src/main/java/africa/semicolon/unicoin/user/UserRepository.java
+++ src/main/java/africa/semicolon/unicoin/user/UserRepository.java
@@ -4,9 +4,10 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User>findByEmailAddressIgnoreCase(String emailAddress);
 
